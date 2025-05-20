1cb2870c981c628562cb6f8cfec608376c02a805
Selina <digitalselina@gmail.com>
2025-05-20 23:55:33 +0600

    About section done

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import Expreiences from "../Home/Expreiences";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <aection>
+      {/* banner */}
+      <div className='bg-[url("/about-background.jpg")] bg-cover bg-no-repeat flex items-center justify-center h-[400px] w-full'>
+        <div>
+          <h1 className="text-5xl font-bold text-white underline underline-offset-4">
+            About Page
+          </h1>
+        </div>
+      </div>
+      {/* Experience section */}
+      <Expreiences />
+    </aection>
+  );
+};
 
-export default About
+export default About;
